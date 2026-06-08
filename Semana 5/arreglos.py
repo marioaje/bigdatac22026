9b8fe88864e9ca4afccc873592f769fe9ea85ca7
marioaje <marioaje@gmail.com>
2026-06-08 08:50:04 -0600

    Create arreglos.py

---
+++ Semana 5/arreglos.py
@@ -0,0 +1,15 @@
+arregloNumeros = [2,34,5,7,8,434,23,87]
+arregloTexto = ["Natalia", "Cristian","Maria Fernanda", "Edith", "Ricardo", "Nathanael"]
+                    # 0     ,  1       ,       2        ,    3
+numero = 12
+print(arregloNumeros)
+print(numero)
+
+print(arregloTexto)
+print(arregloTexto[3])
+
+for itemArreglo in arregloNumeros:
+    print(itemArreglo)
+
+for itemArreglo in arregloTexto:
+    print("hola,", itemArreglo)    
