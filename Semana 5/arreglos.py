8d34a5559fd2b05bd03718fdcbe4d7a70b234ff4
marioaje <marioaje@gmail.com>
2026-06-08 09:20:35 -0600

    Update arreglos.py

--- Semana 5/arreglos.py
+++ Semana 5/arreglos.py
@@ -1,15 +1,45 @@
+# arregloNumeros = [2,34,5,7,8,434,23,87]
+# arregloTexto = ["Natalia", "Cristian","Maria Fernanda", "Edith", "Ricardo", "Nathanael"]
+#                     # 0     ,  1       ,       2        ,    3
+# numero = 12
+# print(arregloNumeros)
+# print(numero)
+
+# print(arregloTexto)
+# print(arregloTexto[3])
+# total = 0
+# totaldos = 0
+# for itemArreglo in arregloNumeros:
+#     print(itemArreglo)
+#     total += itemArreglo
+#     totaldos = totaldos + itemArreglo
+
+
+# print("El total es:", total)
+# print("El total es:", totaldos)
+# totlatres = sum(arregloNumeros)
+# print("El total es:", totlatres)
+# # for itemArreglo in arregloTexto:
+# #     print("hola,", itemArreglo)    
+
+# tengo un reto, me podrian sumar todo lo que esta arregloNumeros     
+# obtener el menor y mayor de este arregloarregloNumeros = [2,34,5,7,8,434,23,87]
+
 arregloNumeros = [2,34,5,7,8,434,23,87]
-arregloTexto = ["Natalia", "Cristian","Maria Fernanda", "Edith", "Ricardo", "Nathanael"]
-                    # 0     ,  1       ,       2        ,    3
-numero = 12
-print(arregloNumeros)
-print(numero)
 
-print(arregloTexto)
-print(arregloTexto[3])
+mayor = arregloNumeros[0]
+menor = arregloNumeros[0]
 
-for itemArreglo in arregloNumeros:
-    print(itemArreglo)
+for items in arregloNumeros:
+    if items > mayor:
+        mayor = items
 
-for itemArreglo in arregloTexto:
-    print("hola,", itemArreglo)    
+    if items < menor:
+        menor = items
+
+print ("numero mayor: ", mayor)        
+print ("numero menor: ", menor)        
+
+
+print ("numero mayor: ", max(arregloNumeros))        
+print ("numero menor: ", min(arregloNumeros))       
